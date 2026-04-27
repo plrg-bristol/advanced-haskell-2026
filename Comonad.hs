3a96a37c0247832b1cf2b52e755f85f3a0509901
Samantha Frohlich <sf16540@my.bristol.ac.uk>
2026-04-27 14:01:33 +0100

    more comonads

--- Comonad.hs
+++ Comonad.hs
@@ -1,6 +1,7 @@
 
-module Comonad where
+{-# LANGUAGE InstanceSigs #-}
 
+module Comonad where
 
 import Prelude hiding (Monad(..))
 
@@ -35,7 +36,58 @@ class (Functor m, Applicative m) => Monad m where
 class Functor w => Comonad w where
   extract :: w e -> e
   duplicate :: w e -> w (w e)
-  extend :: w d -> (w d -> e) -> w e
+  extend :: (w d -> e) -> w d -> w e
 
-  duplicate we = extend we id
-  extend = undefined
+  duplicate we = extend id we
+  extend k wd = fmap k $ duplicate wd
+
+-- comonad laws
+-- extend extract      = id
+-- extract . extend f  = f
+-- extend f . extend g = extend (f . extend g)
+-- extract . duplicate      = id
+-- fmap extract . duplicate = id
+-- duplicate . duplicate    = fmap duplicate . duplicate
+
+data Infinite a = Cons a (Infinite a) deriving Show
+
+exampleInf :: Infinite Int
+exampleInf = Cons 1 (Cons 2 (Cons 3 exampleInf))
+
+instance Functor Infinite where
+  fmap :: (a -> b) -> Infinite a -> Infinite b
+  fmap f (Cons a i) = Cons (f a) (fmap f i)
+
+data Tape a = Tape (Infinite a) a (Infinite a) deriving Show
+
+instance Functor Tape where
+  fmap :: (a -> b) -> Tape a -> Tape b
+  fmap f (Tape l a r) = Tape (fmap f l) (f a) (fmap f r)
+
+-- fmap laws
+--Identity
+-- fmap id == id
+-- Composition
+-- fmap (f . g) == fmap f . fmap g
+
+instance Comonad Tape where
+  extract :: Tape a -> a
+  extract (Tape _ a _) = a
+
+  duplicate :: Tape a -> Tape (Tape a)
+  duplicate ta = Tape undefined ta undefined
+
+-- unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+unfold :: (a -> (a,a)) -> a -> Infinite a
+unfold f a =
+  let (a1, a2) = f a
+  in Cons a1 (unfold f a2)
+
+allLefts :: Tape a -> Infinite (Tape a)
+allLefts t = unfold undefined undefined
+
+helperL :: Tape a -> Tape a
+helperL (Tape (Cons x xs) a rs) = Tape xs x (Cons a rs)
+
+helperR :: Tape a -> Tape a
+helperR (Tape ls a (Cons x xs)) = Tape (Cons a ls) x xs
